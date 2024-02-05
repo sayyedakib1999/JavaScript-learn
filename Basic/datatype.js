c91e02a63ab3fdbd2ae298e0bec71302c16c9eff
Akib Sayyed <88134276+sayyedakib1999@users.noreply.github.com>
2024-02-05 17:19:44 +0000

    updated

--- Basic/datatype.js
+++ Basic/datatype.js
@@ -10,10 +10,31 @@
 
 let name = "Akib" //String Datatype
 let age = 54 //Number Datatype
-let isLoggedIn = True //Boolean Datatype 
+// let isLoggedIn = True //Boolean Datatype 
 
-// Range Of The Number is 2 to the power of 52 or 53 
-// bigInt jab aap ka number 2 to the power 53 say bada hai jab hum bigInt Datatype use karegay 
-// string is also a datatype hum isko single quote mai ( '' ) ya doubble quote ( "" )mai likh saktay  
+// Range Of The Number is 2 to the power of 52 or 53.
+
+// bigInt jab aap ka number 2 to the power 53 say bada hai jab hum bigInt Datatype use karegay. 
+
+// string is also a datatype hum isko single quote mai ( '' ) ya doubble quote ( "" )mai likh saktay. 
+
 // boolean basical yeh True or false wali condition hai (Haa Ya Naa) (Yes Or No).
-// null kai stand alone value hai.
+
+// null kai stand alone value hai, also null is reprenstation of empty value. also null is object.
+ 
+// undefined => jab hum value define nahi kar tai.
+
+// symbol to find uniqueness 
+
+//object => (In futher Video)
+
+console.log(typeof undefined); // It shows the type of datatype , wheather it is string, number, boolean etc ...
+
+/* when we try to find out the typeof undefine it is (undefine), 
+but when we try to find out the typeof null then it show the object,
+some people says that it is error & some people says the it is santerd formate. */
+
+if (true) {
+    var x = 5;
+  }
+  console.log(x); // x is 
