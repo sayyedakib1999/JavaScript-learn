2f90845f5afd84240654b0a5a24b66e782816c9b
Akib Sayyed <88134276+sayyedakib1999@users.noreply.github.com>
2024-02-05 16:10:53 +0000

    updated

---
+++ Basic/datatype.js
@@ -0,0 +1,10 @@
+"String"; // yeh treat honga newer version of Javascript.
+
+// alert("Hello") //we are using nodeJS not Browser
+
+/* console.log(3+
+    3) Code must be readable */ 
+
+  
+    // console.log("AKib"); // code must be redable , semicolon is optional ...
+
