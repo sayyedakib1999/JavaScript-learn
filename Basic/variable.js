a0b00970a21271c640e5fc8e72e770df78907db0
Akib Sayyed <88134276+sayyedakib1999@users.noreply.github.com>
2024-02-05 14:12:54 +0000

    new file

---
+++ Basic/variable.js
@@ -0,0 +1,7 @@
+accId = 14525562 //This Constant (If We Declare We are not able to change the values)
+let accEmail = "akibsayyed@gmail.com" //This Let
+var accPass = "12345"
+accCity = "Ahmednagar" 
+
+
+console.log(accId);
