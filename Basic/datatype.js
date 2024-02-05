c867103b7cd618244d669e1064241c3e520e2ca0
Akib Sayyed <88134276+sayyedakib1999@users.noreply.github.com>
2024-02-05 16:55:59 +0000

    updated

--- Basic/datatype.js
+++ Basic/datatype.js
@@ -11,3 +11,8 @@
 let name = "Akib" //String Datatype
 let age = 54 //Number Datatype
 let isLoggedIn = True //Boolean Datatype 
+
+// Range Of The Number is 2 to the power of 52 or 53 
+//bigInt jab aap ka number 2 to the power 53 say bada hai jab hum bigInt Datatype use karegay 
+// string is also a datatype hum isko single quote mai ( '' ) ya doubble quote ( "" )mai likh saktay  
+// boolean basical yeh True or false wali condition hai (Haa Ya Naa) (Yes Or No)
