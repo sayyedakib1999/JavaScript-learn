538b2fcb5344c83690225467bfc0bf08ebd68b3d
Akib Sayyed <88134276+sayyedakib1999@users.noreply.github.com>
2024-02-06 06:30:10 +0000

    Updated

---
+++ Basic/datatypeconverstion.js
@@ -0,0 +1,6 @@
+let score = 33
+console.log(typeof score); //hum isko directly bhi likh saktai hai orr
+
+console.log(typeof (score)); // yafir hum is ko as a method bhi likh saktay. 
+
+// Q What Is The Diffreance between  { typeof value  & typof(value) }.
