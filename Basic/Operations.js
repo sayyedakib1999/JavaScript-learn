d096fbdbdb5ae9553af1e4a251f036e7abc008b4
Akib Sayyed <88134276+sayyedakib1999@users.noreply.github.com>
2024-02-06 13:27:11 +0000

    Updated

---
+++ Basic/Operations.js
@@ -0,0 +1,18 @@
+// chapter No 7: Operations 
+
+
+// Operations ka matlab koe bhi task perform kr na 
+// for eg 
+// 2 + 2 | 3-2 | to contact the string ( String ko join kr na )
+
+// let str1 = "Hello"
+// let str2 = " Akib"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
