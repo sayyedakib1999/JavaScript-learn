3234ab8929f7018c586374889fdeaa5704a28dda
Akib Sayyed <88134276+sayyedakib1999@users.noreply.github.com>
2024-02-08 06:48:46 +0000

    updated

---
+++ Basic/comparison.js
@@ -0,0 +1,10 @@
+// console.log(2 < 1);
+// console.log("2" > 1); not allowed to comaparion between 2 data type
+
+/* console.log(null > 0);
+ console.log(null == 0);
+ console.log(null >= 0);
+
+
+ console.log(undefined == 0); */ //we are trying to avoid this type of operatios 
+
